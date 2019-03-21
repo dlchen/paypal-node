da791872a6e98d9b400c387969f7a3932362c9c9
dlchen <dan.zot@gmail.com>
2019-03-20 22:18:24 -0700

    feat: return 250 transactions

--- api/mockDb.js
+++ api/mockDb.js
@@ -1,11 +1,18 @@
 'use strict';
 
-const data = require('./mockData.json');
-
 class Database {
 
   async query() {
-    return data;
+    return {
+      transactions: new Array(250).fill(null).map((_, i) => {
+        return {
+          name: `Person ${i}`,
+          date: new Date(2018, 1, 1, i),
+          amount: i + 0.02,
+          currency: 'USD'
+        }
+      })
+    };
   }
 }
 
